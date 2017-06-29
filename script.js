92ad18685adc7c61547420ddc1fe2cbed2b78cfb
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2017-06-29 23:35:17 +0200

    nice background for everyone

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const apiVersion = '7.13.1'
+
 const getJSON = (url, callback) => {
     var xhr = new XMLHttpRequest()
     xhr.open('GET', url, true)
@@ -21,12 +23,14 @@ getJSON('php/data.json', (err, data) => {
         .forEach(member => {
             inner += `
                 <div class="mdc-card member">
-                    <section class="mdc-card__media" style="background-image: url(${member.matchobject.championId})"></section>
-                    <section class="mdc-card__primary">
-                        <div class="member__summoner-icon" style="background-image: url(http://ddragon.leagueoflegends.com/cdn/7.13.1/img/profileicon/${member.summonerobject.profileIconId}.png )"></div>
-                        <h1 class="mdc-card__title mdc-card__title--large">${member.l4uname}</h1>
-                        <h2 class="mdc-card__subtitle">${member.task}</h2>
+                    <section class="mdc-card__media" style="background-image: url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${member.matchobject.championName}_0.jpg)">
+                        <section class="mdc-card__primary member__title">
+                            <div class="member__summoner-icon" style="background-image: url(http://ddragon.leagueoflegends.com/cdn/${apiVersion}/img/profileicon/${member.summonerobject.profileIconId}.png )"></div>
+                            <h1 class="mdc-card__title mdc-card__title--large">${member.l4uname}</h1>
+                            <h2 class="mdc-card__subtitle">${member.task}</h2>
+                        </section>
                     </section>
+
                 </div>`
         })
     document.getElementById('membersContainer').innerHTML = inner
